dd399935aa7ad73a53a35ec0bbc488653b4825bb
Richard <56603984+Reach-AM@users.noreply.github.com>
2021-11-21 09:19:57 -0600

    Add files via upload

---
+++ src/yt_query.py
@@ -0,0 +1,23 @@
+import re
+import urllib.request
+import pafy
+
+def playAlbum():
+    html = urllib.request.urlopen('https://www.youtube.com/playlist?list=PLxws1M2-zjAPNbXeJrH_TTstwSQdm9KZ4')
+    video_ids = re.findall(r'watch\?v=(\S{11})', html.read().decode())
+
+    links = []
+    for i in video_ids:
+        if i not in links: links.append(i)
+
+    for i,link in enumerate(links):
+        links[i] = 'https://www.youtube.com/watch?v=' + link
+
+    album = []
+    for link in links:
+        song = []
+        media = pafy.new(link)
+        best = media.getbestaudio()
+        song.append(media.title)
+        song.append(best.url)
+        song.append(media._length)
