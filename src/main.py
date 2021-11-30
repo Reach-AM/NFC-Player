b79f261437940ed7ba85a1e8e52fe3b4bb1389d4
Richard <56603984+Reach-AM@users.noreply.github.com>
2021-11-30 13:48:25 -0600

    Shuffle + Repeat methods

--- src/main.py
+++ src/main.py
@@ -7,7 +7,9 @@ import time
 
 from player import *
 
-from PyQt5.QtCore import QTimer
+from PyQt5 import QtCore
+from PyQt5 import QtGui
+#from PyQt5.QtCore import QTimer
 from PyQt5.QtGui import QFont, QIcon, QPixmap
 from PyQt5.QtWidgets import (
     QApplication,
@@ -22,53 +24,75 @@ playlist = 'https://www.youtube.com/playlist?list=PLxws1M2-zjAPNbXeJrH_TTstwSQdm
 
 class Window(QWidget):
     player = None
-    #lTitle = None
-    bBack = bPlayPause = bNext = None
+    bBack = bPlayPause = bNext = bShuffle = bRepeat = None
 
     def __init__(self, playlist):
         super().__init__()
         lTitle = QLabel()
-        self.player = Player(playlist, lTitle)
+        self.bPlayPause = QPushButton("⏸️");
+        self.player = Player(playlist, lTitle, self.bPlayPause)
 
+        self.setStyleSheet("background-color: rgb(23,23,23); color : rgb(232, 232, 232);")
         self.setWindowTitle("Daft Punk")
-        self.setFixedSize(500, 600)
+        self.setFixedSize(520, 700)
 
         # Create layouts
         vlayout = QVBoxLayout()
         hlayout = QGridLayout()
+        hlayout2 = QGridLayout()
 
         # Set image in label
         label = QLabel()
         pixmap = QPixmap('assets/img/dft.jpg')
         label.setPixmap(pixmap)
 
-        # Set title label
+        # Set labels
+        lAlbum= QLabel('Daft Punk - Random Access Memories')
         lTitle.setText(self.player.playlist[self.player.no]['title'])
+        lAlbum.setFont(QFont('Lato', 14))
+        lAlbum.setAlignment(QtCore.Qt.AlignCenter)
+        lTitle.setFont(QFont('Lato', 10))
+        lTitle.setAlignment(QtCore.Qt.AlignCenter)
 
         # Back button
         self.bBack = QPushButton("⏮️");
-        self.bBack.setFont(QFont('Arial', 23))
+        self.bBack.setFont(QFont('Lato', 23))
         self.bBack.clicked.connect(self.back_pressed)
 
         # Play / Pause button
-        self.bPlayPause = QPushButton("⏯");
-        self.bPlayPause.setFont(QFont('Arial', 23))
+        self.bPlayPause.setFont(QFont('Lato', 23))
         self.bPlayPause.clicked.connect(self.playpause_pressed)
 
         # Next button
         self.bNext = QPushButton("⏭️");
-        self.bNext.setFont(QFont('Arial', 23))
+        self.bNext.setFont(QFont('Lato', 23))
         self.bNext.clicked.connect(self.next_pressed)
+
+        # Shuffle button
+        self.bShuffle = QPushButton("🔀");
+        self.bShuffle.setFont(QFont('Lato', 20))
+        self.bShuffle.clicked.connect(self.shuffle_pressed)
+
+        # Repeat button
+        self.bRepeat = QPushButton("🔁️");
+        self.bRepeat.setFont(QFont('Lato', 20))
+        self.bRepeat.clicked.connect(self.repeat_pressed)
 
         # Add widgets to the hlayout
         hlayout.addWidget(self.bBack, 0, 0)
         hlayout.addWidget(self.bPlayPause, 0, 1)
         hlayout.addWidget(self.bNext, 0, 2)
 
+        # Add widgets to the hlayout2
+        hlayout2.addWidget(self.bShuffle, 0, 0)
+        hlayout2.addWidget(self.bRepeat, 0, 1)
+
         # Add widgets to the vlayout
+        vlayout.addWidget(lAlbum)
         vlayout.addWidget(label)
         vlayout.addWidget(lTitle)
         vlayout.addLayout(hlayout)
+        vlayout.addLayout(hlayout2)
 
         # Set the layout on the application's window
         self.setLayout(vlayout)
@@ -88,16 +112,19 @@ class Window(QWidget):
         self.bPlayPause.setEnabled(False)
         QTimer.singleShot(1000, lambda: self.bPlayPause.setDisabled(False))
 
+    def shuffle_pressed(self):
+        self.player.shuffle()
+        self.bShuffle.setEnabled(False)
+        QTimer.singleShot(1000, lambda: self.bShuffle.setDisabled(False))
+
+    def repeat_pressed(self):
+        self.player.repeat()
+        self.bRepeat.setEnabled(False)
+        QTimer.singleShot(1000, lambda: self.bRepeat.setDisabled(False))
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    QtGui.QFontDatabase.addApplicationFont('assets/font/Lato.ttf')
     window = Window(playlist)
     window.show()
     sys.exit(app.exec_())
-
-#album = Album(playlist)
-
-#for song in Album.playlist:
-#    audio = vlc.MediaPlayer(song['url'])
-#    print(song['title'])
-#    audio.play()
-#    time.sleep(song['length'])
--- src/player.py
+++ src/player.py
@@ -10,13 +10,15 @@ from PyQt5.QtWidgets import QLabel
 class Player:
     tracks = []
     playlist = []
-    label = None
+    label = playbutton = None
     player = None
     current = None
     no = 0
+    shff = rept = False
 
-    def __init__(self, url, label):
+    def __init__(self, url, label, playbutton):
             self.label = label
+            self.playbutton = playbutton
             html = urllib.request.urlopen(url)
             video_ids = re.findall(r'watch\?v=(\S{11})', html.read().decode())
 
@@ -30,9 +32,10 @@ class Player:
 
                     song = {
                         'number': i,
-                        'title': media.title,
+                        'title': media.title.split(' - ')[1].split(' (')[0],
                         'url': best.url,
-                        'length': media._length
+                        'length': media._length,
+                        'og': 'https://www.youtube.com/watch?v=' + link
                     }
                     links.append(link)
                     self.tracks.append(song)
@@ -44,15 +47,25 @@ class Player:
             self.playSong()
             #if shuffle: random.shuffle(self.playlist)
 
+    def __str__(self):
+        str = '['
+        for i, song in enumerate(self.playlist):
+            if i < len(self.playlist)-1:
+                str += song['title'] + ', '
+            else:
+                str += song['title'] + ']'
+        return str
 
     def playPause(self):
         if self.player.is_playing():
             self.player.pause()
             self.current.stop()
+            self.playbutton.setText("▶")
         else:
             self.player.play()
             remaining = self.playlist[self.no]['length'] * 1000 - self.player.get_time() + 1000
             self.current.start(remaining)
+            self.playbutton.setText("⏸")
 
     def playNext(self, auto = True):
         self.no += 1
@@ -60,15 +73,22 @@ class Player:
         if self.no < len(self.playlist):
             self.player.stop()
             self.playSong()
-        else: self.no -+ 1
+        else:
+            self.no = 0
+            self.player.stop()
+            if self.rept: self.playSong()
+            else:
+                self.playSong(False)
+                self.playbutton.setText("▶️")
 
-    def playSong(self):
+    def playSong(self, play = True):
         num = self.no
         self.label.setText(self.playlist[num]['title'])
         self.player = vlc.MediaPlayer(self.playlist [num]['url'])
-        if self.no == num:
+        if self.no == num and play:
             self.player.play()
             self.current.start((self.playlist[num]['length']+1)*1000)
+            self.playbutton.setText("⏸")
             self.current.singleShot(5000, lambda: self.vlc_success_check(num))
 
     def playBack(self):
@@ -82,7 +102,24 @@ class Player:
     def vlc_success_check(self, num):
         if not self.player.is_playing() and self.no == num:
             self.current.stop()
-            self.player = vlc.MediaPlayer(self.playlist [num]['url'])
+            self.playlist[num]['url'] = pafy.new(self.playlist[num]['og']).getbestaudio().url
+            self.player = vlc.MediaPlayer(self.playlist[num]['url'])
             self.player.play()
             self.current.start((self.playlist[self.no]['length']+1)*1000)
             self.current.singleShot(5000, lambda: self.vlc_success_check(num))
+
+    def shuffle(self):
+        if not self.shff:
+            nowp = self.playlist.pop(self.no)
+            random.shuffle(self.playlist)
+            self.playlist.insert(0,nowp)
+            self.no = 0
+            self.shff = True
+        else:
+            self.no = self.playlist[self.no]['number']
+            self.playlist = self.tracks[:]
+            self.shff = False
+
+    def repeat(self):
+        if self.rept: self.rept = False
+        else: self.rept = True
